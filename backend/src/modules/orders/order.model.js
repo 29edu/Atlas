742b10dffe30f097d644294c5ff1db24ac2db55a
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2026-03-19 21:54:11 +0530

    updated

--- backend/src/modules/orders/order.model.js
+++ backend/src/modules/orders/order.model.js
@@ -50,7 +50,7 @@ const orderSchema = new mongoose.Schema({
     shippingAddress: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Address",
-        required: true
+        required: true;
     }
 
 }, {
